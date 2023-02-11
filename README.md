87a4f2450378864145a466c77c57475f6ef20b50
nathanielBellamy <nbschieber@gmail.com>
2023-02-11 11:15:14 -0800

    init gem

---
+++ README.md
@@ -0,0 +1,7 @@
+# rustby
+
+### Rust Within Ruby
+
+## License
+
+The gem is available as open source under the terms of the [MIT License](https://opensource.org/licenses/MIT).
---
+++ spec/rustby_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Rustby do
+  it "has a version number" do
+    expect(Rustby::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
