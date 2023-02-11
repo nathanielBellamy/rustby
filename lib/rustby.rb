5e422f20b52ab9164f265f16202a3b421532b520
nathanielBellamy <nbschieber@gmail.com>
2023-02-11 13:35:53 -0800

    initial commit

--- lib/rustby.rb
+++ lib/rustby.rb
@@ -4,5 +4,18 @@ require_relative "rustby/version"
 
 module Rustby
   class Error < StandardError; end
-  # Your code goes here...
+  pp "Count me in!"
+
+  sleep 2
+
+  pp "And a..."
+
+  sleep 0.5
+
+  (5..8).each do |n|
+    pp n
+    sleep 0.5
+  end
+
+  pp "Go!"
 end
