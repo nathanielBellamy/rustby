c90659d3db1f3d405e7866da0997cdcc442c9c44
nathanielBellamy <nbschieber@gmail.com>
2023-02-11 21:35:41 -0800

    fix gemfile, have fun in specs

--- rustby.gemspec
+++ rustby.gemspec
@@ -8,17 +8,13 @@ Gem::Specification.new do |spec|
   spec.authors       = ["nathanielBellamy"]
   spec.email         = ["nbschieber@gmail.com"]
 
-  spec.summary       = "TODO: Write a short summary, because RubyGems requires one."
-  spec.description   = "TODO: Write a longer description or delete this line."
-  spec.homepage      = "TODO: Put your gem's website or public repo URL here."
+  spec.summary       = "An Embedding of Rust into Ruby using rutie"
+  spec.description   = "Benchmark performance of Pure Ruby vs Ruby + Embedded Rust"
   spec.license       = "MIT"
   spec.required_ruby_version = Gem::Requirement.new(">= 2.3.0")
 
-  spec.metadata["allowed_push_host"] = "TODO: Set to 'http://mygemserver.com'"
-
-  spec.metadata["homepage_uri"] = spec.homepage
-  spec.metadata["source_code_uri"] = "TODO: Put your gem's public repo URL here."
-  spec.metadata["changelog_uri"] = "TODO: Put your gem's CHANGELOG.md URL here."
+  spec.metadata["allowed_push_host"] = "git@github.com:nathanielBellamy/rustby.git"
+  spec.metadata["source_code_uri"] = "https://github.com/nathanielBellamy/rustby"
 
   # Specify which files should be added to the gem when it is released.
   # The `git ls-files -z` loads the files in the RubyGem that have been added into git.
--- spec/rustby_spec.rb
+++ spec/rustby_spec.rb
@@ -5,7 +5,49 @@ RSpec.describe Rustby do
     expect(Rustby::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  context "when doing things" do
+    let(:useful_helper_dbl) { instance_double(UsefulHelper) }
+
+    before do
+      allow(UsefulHelper).to receive(:new).and_return(useful_helper_dbl)
+      allow(useful_helper_dbl).to receive(:provide_aid).
+                                  and_return("You have been helped by an imposter.")
+    end
+
+    it "does something useful" do
+      count = 0
+      message = ''
+      expect do
+        (0..3).each do |n|
+          case n
+          when 1, 2
+            message = UsefulHelper.new.provide_aid
+            expect(message).to eq("You have been helped by an imposter.")
+          when 3
+            message = UsefulHelper.provide_aid
+          end
+          count += 1
+        end
+      end.to change { count }.from(0).to(4)
+
+      expect(useful_helper_dbl).to have_received(:provide_aid).exactly(2).times
+      expect(message).to eq("You have been helped by the singleton.")
+    end
+
+    it "and also other useful things" do
+      allow(UsefulHelper).to receive(:new).and_call_original
+
+      expect(UsefulHelper.new.provide_aid).to eq("You have been helped.")
+    end
+  end
+
+  class UsefulHelper
+    def self.provide_aid
+      "You have been helped by the singleton."
+    end
+
+    def provide_aid
+      "You have been helped."
+    end
   end
 end
